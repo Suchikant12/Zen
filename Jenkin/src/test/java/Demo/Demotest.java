af0b8d07804bc47d17184fcb43e4f5594884e99a
Suchikant12 <suchi.bitu@gmail.com>
2023-12-16 10:58:50 +0530

    1st push

---
+++ Jenkin/src/test/java/Demo/Demotest.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Demotest {
+	
+	@Test
+	public void vtest()
+	{
+		System.out.println("Hello");
+	}
+
+}
---
+++ Jenkin/src/test/java/Demo/Sampletest.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Sampletest {
+	@Test
+	public void samptest()
+	{
+		System.out.println("Hello");
+	}
+
+}
